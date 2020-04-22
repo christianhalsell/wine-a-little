fa55dac1487ab5d800d97a6faa29c00798817bfe
Christian Halsell <shinobiwulf@gmail.com>
2020-04-22 13:55:10 -0700

    Homepage scaffolding

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,6 +3,42 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className="homepage">
+    <div className="banner">
+      Banner
+    </div>
+
+    <div className="homepage-content">
+      <div className="homepage-callouts">
+        <div className="homepage-callout">
+          left
+        </div>
+        <div className="homepage-callout">
+          center
+        </div>
+        <div className="homepage-callout">
+          right
+        </div>
+      </div>
+
+      <div className="homepage-deals">
+        <div className="homepage-deal">
+          A
+        </div>
+        <div className="homepage-deal">
+          B
+        </div>
+        <div className="homepage-deal">
+          C
+        </div>
+        <div className="homepage-deal">
+          D
+        </div>
+      </div>
+    </div>
+
+    <footer className="footer">
+      footer
+    </footer>
   </div>
 )
 
