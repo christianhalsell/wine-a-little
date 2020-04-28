03d43976d94b6b5d682a169d18a3efcbf0a97992
Christian Halsell <shinobiwulf@gmail.com>
2020-04-28 14:02:37 -0700

    Moved homepage styles into components structure

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './homepage.styles.scss';
 
 import Banner from '../../components/banner/banner.component';
 import HomepageContent from '../../components/homepage-content/homepage-content.component';
