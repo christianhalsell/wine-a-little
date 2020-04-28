02a9be8173ea386cc52b7057b02a7bc08bff7fdf
Christian Halsell <shinobiwulf@gmail.com>
2020-04-28 10:23:53 -0700

    Create banner component

---
+++ src/components/banner/banner.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './banner.styles.scss';
+
+const Banner = () => (
+  <div className="banner">
+    <div className="banner-content">
+      <div className="logo">
+        Logo
+      </div>
+
+      <nav className="navigation">
+        Shop
+        Contact
+        Sign In
+      </nav>
+    </div>
+  </div>
+);
+
+export default Banner;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './homepage.styles.scss';
 
+import Banner from '../../components/banner/banner.component';
+
 import RedBottle from './images/redwine-cropped@2x.png';
 import WhiteBottle from './images/whitewine-cropped@2x.png';
 import ChampagneBottle from './images/champagne-cropped@2x.png';
@@ -13,73 +15,60 @@ import DealD from './images/faire-la-fete-cremant-de-limoux-brout-rose@2x.png'
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="banner">
-      <div className="banner-content">
-        <div className="logo">
-          Logo
-        </div>
-
-        <nav className="navigation">
-          Shop
-          Contact
-          Sign In
-        </nav>
-      </div>
-    </div>
-
+    <Banner />
     <div className="homepage-content">
       <div className="homepage-callouts">
-        <div className="homepage-callout">
-          <img className="callout-image" src={RedBottle} alt="red wine bottle" />
+        <div className="navigation-card">
+          <img className="navigation-card-image" src={RedBottle} alt="red wine bottle" />
           <h2>Red Wine</h2>
-          <div className="callout-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <div className="navigation-card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Praesent a nisi justo. Pellentesque nec efficitur odio.</div>
-          <div className="callout-link">View All</div>
+          <div className="navigation-card-link">View All</div>
         </div>
 
-        <div className="homepage-callout">
-          <img className="callout-image" src={WhiteBottle} alt="white wine bottle" />
+        <div className="navigation-card">
+          <img className="navigation-card-image" src={WhiteBottle} alt="white wine bottle" />
           <h2>White Wine</h2>
-          <div className="callout-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <div className="navigation-card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Praesent a nisi justo. Pellentesque nec efficitur odio.</div>
-          <div className="callout-link">View All</div>
+          <div className="navigation-card-link">View All</div>
         </div>
 
-        <div className="homepage-callout">
-          <img className="callout-image" src={ChampagneBottle} alt="champagne bottle" />
+        <div className="navigation-card">
+          <img className="navigation-card-image" src={ChampagneBottle} alt="champagne bottle" />
           <h2>Champagne</h2>
-          <div className="callout-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <div className="navigation-card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Praesent a nisi justo. Pellentesque nec efficitur odio.</div>
-          <div className="callout-link">View All</div>
+          <div className="navigation-card-link">View All</div>
         </div>
       </div>
 
       <div className="homepage-deals">
-        <div className="homepage-deal">
-          <img className="deal-image" src={DealA} alt="" />
-          <div className="deal-text">Daou Vineyards Pessimist Red Blend</div>
-          <div className="deal-price">$19.95</div>
+        <div className="product-item">
+          <img className="product-item-image" src={DealA} alt="" />
+          <div className="product-item-text">Daou Vineyards Pessimist Red Blend</div>
+          <div className="product-item-price">$19.95</div>
           <button>Add to Cart</button>
         </div>
 
-        <div className="homepage-deal">
-          <img className="deal-image" src={DealB} alt="" />
-          <div className="deal-text">Brick and Mortar <br />Rose</div>
-          <div className="deal-price">$24.95</div>
+        <div className="product-item">
+          <img className="product-item-image" src={DealB} alt="" />
+          <div className="product-item-text">Brick and Mortar <br />Rose</div>
+          <div className="product-item-price">$24.95</div>
           <button>Add to Cart</button>
         </div>
 
-        <div className="homepage-deal">
-          <img className="deal-image" src={DealC} alt="" />
-          <div className="deal-text">Jean Marc Bugaud Morgon les Charmes 2018</div>
-          <div className="deal-price">$22.95</div>
+        <div className="product-item">
+          <img className="product-item-image" src={DealC} alt="" />
+          <div className="product-item-text">Jean Marc Bugaud Morgon les Charmes 2018</div>
+          <div className="product-item-price">$22.95</div>
           <button>Add to Cart</button>
         </div>
 
-        <div className="homepage-deal">
-          <img className="deal-image" src={DealD} alt="" />
-          <div className="deal-text">Faire la Fete Cremant de Limoux Brout Rose</div>
-          <div className="deal-price">$29.95</div>
+        <div className="product-item">
+          <img className="product-item-image" src={DealD} alt="" />
+          <div className="product-item-text">Faire la Fete Cremant de Limoux Brout Rose</div>
+          <div className="product-item-price">$29.95</div>
           <button>Add to Cart</button>
         </div>
       </div>
