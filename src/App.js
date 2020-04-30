d9abe4216da77ae232b640c655d2d509e9f6d712
Christian Halsell <shinobiwulf@gmail.com>
2020-04-30 09:37:43 -0700

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const TestPage = () => (
+  <div>
+    <h1>Test Page</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/test" component={TestPage} />
+      </Switch>
     </div>
   );
 }
