baae669d8c58e394718117bcb652cd690d574f3f
Christian Halsell <shinobiwulf@gmail.com>
2020-05-04 17:18:16 -0700

    Shop styling

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import './shop.styles.scss';
 import SHOP_DATA from './shop.data';
 
 import Banner from '../../components/banner/banner.component';
+import ProductItem from '../../components/product-item/product-item.component';
 
 class ShopPage extends React.Component {
   constructor() {
@@ -18,20 +20,26 @@ class ShopPage extends React.Component {
     return (
       <div>
         <Banner bannerNumber="2" />
-        {
-          inventory.map((item) => (
-            <div>
-              <div key={item.id}>{item.title}</div>
-              <ul>
-                {
-                  item.items.map((wine) => (
-                    <li key={wine.id}>{wine.text} ({wine.price})</li>
-                  ))
-                }
-              </ul>
-            </div>
-          ))
-        }
+        <div className="shop-content">
+          <div className="shop-content-center">
+            {
+              inventory.map(({ id, title, items }) => (
+                <div>
+                  <h2 key={id}>{title}</h2>
+                  <div className="product-items">
+                    {
+                      items
+                        .filter((item, idx) => idx < 4)
+                        .map(({ id, ...wine }) => (
+                          <ProductItem key={id} {...wine} />
+                        ))
+                    }
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </div>
     );
   }
--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -13,49 +13,49 @@ const SHOP_DATA = [
       },
       {
         id: 2,
-        imageUrl: 'https://www.christianhalsell.com/images/paraduxx-proprietary-red-2016@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/paraduxx-proprietary-red-2016@2x.jpg',
         linkUrl: '',
         price: '$39.99',
         text: 'Paraduxx Proprietary Red 2016'
       },
       {
         id: 3,
-        imageUrl: 'https://www.christianhalsell.com/images/the-investor-red-wine@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/the-investor-red-wine@2x.jpg',
         linkUrl: '',
         price: '$37.99',
         text: 'Stags\' Leap Winery The Investor Red Wine 2017'
       },
       {
         id: 4,
-        imageUrl: 'https://www.christianhalsell.com/images/conundrum-red-blend-2017@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/eight-years-in-the-desert-2018@2x.jpg',
+        linkUrl: '',
+        price: '$44.99',
+        text: '8 Years in the Desert Red Blend'
+      },
+      {
+        id: 5,
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/conundrum-red-blend-2017@2x.jpg',
         linkUrl: '',
         price: '$23.99',
         text: 'Conundrum Red Blend'
       },
       {
-        id: 5,
-        imageUrl: 'https://www.christianhalsell.com/images/eight-years-in-the-desert-2018@2x.jpg',
-        linkUrl: '',
-        price: '$44.99',
-        text: '8 Years in the Desert'
-      },
-      {
         id: 6,
-        imageUrl: 'https://www.christianhalsell.com/images/allegrini-palazzo-della-torre-2015@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/allegrini-palazzo-della-torre-2015@2x.jpg',
         linkUrl: '',
         price: '$19.99',
         text: 'Allegrini Palazzo della Torre 2015'
       },
       {
         id: 7,
-        imageUrl: 'https://www.christianhalsell.com/images/apothic-red-blend-2018@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/apothic-red-blend-2018@2x.jpg',
         linkUrl: '',
         price: '$10.99',
         text: 'Apothic Red Blend 2018'
       },
       {
         id: 8,
-        imageUrl: 'https://www.christianhalsell.com/images/19-crimes-red-blend-2018@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/19-crimes-red-blend-2018@2x.jpg',
         linkUrl: '',
         price: '$10.99',
         text: '19 Crimes Red Blend 2018'
@@ -69,56 +69,56 @@ const SHOP_DATA = [
     items: [
       {
         id: 1,
-        imageUrl: 'https://www.christianhalsell.com/images/whitehaven-sauvingnon-blanc-2019@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/whitehaven-sauvingnon-blanc-2019@2x.jpg',
         linkUrl: '',
         price: '$17.99',
         text: 'Whitehaven Sauvingnon Blanc 2019'
       },
       {
         id: 2,
-        imageUrl: 'https://www.christianhalsell.com/images/kim-crawford-sauvingnon-blanc-2019@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/kim-crawford-sauvingnon-blanc-2019@2x.jpg',
         linkUrl: '',
         price: '$16.99',
         text: 'Kim Crawford Sauvingnon Blanc 2019'
       },
       {
         id: 3,
-        imageUrl: 'https://www.christianhalsell.com/images/oyster-bay-marlborough-savingnon-blanc@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/oyster-bay-marlborough-savingnon-blanc@2x.jpg',
         linkUrl: '',
         price: '$15.99',
         text: 'Oyster Bay Marlborough Sauvingnon Blanc'
       },
       {
         id: 4,
-        imageUrl: 'https://www.christianhalsell.com/images/rombauer-chardonnay@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/rombauer-chardonnay@2x.jpg',
         linkUrl: '',
         price: '$39.99',
         text: 'Rombauer Chardonnay 2018'
       },
       {
         id: 5,
-        imageUrl: 'https://www.christianhalsell.com/images/grgich-hills-estate-chardonnay@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/grgich-hills-estate-chardonnay@2x.jpg',
         linkUrl: '',
         price: '$39.99',
         text: 'Grgich Hills Estate Chardonnay'
       },
       {
         id: 6,
-        imageUrl: 'https://www.christianhalsell.com/images/bread-and-butter-chardonnay@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/bread-and-butter-chardonnay@2x.jpg',
         linkUrl: '',
         price: '$13.99',
         text: 'Bread & Butter Chardonnay'
       },
       {
         id: 7,
-        imageUrl: 'https://www.christianhalsell.com/images/raeburn-chardonnay@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/raeburn-chardonnay@2x.jpg',
         linkUrl: '',
         price: '$16.99',
         text: 'Raeburn Chardonnay 2018'
       },
       {
         id: 8,
-        imageUrl: 'https://www.christianhalsell.com/images/talbott-kali-hart-chardonnay@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/talbott-kali-hart-chardonnay@2x.jpg',
         linkUrl: '',
         price: '$17.99',
         text: 'Talbott Kali-Hart Chardonnay'
@@ -132,56 +132,56 @@ const SHOP_DATA = [
     items: [
       {
         id: 1,
-        imageUrl: 'https://www.christianhalsell.com/images/zonin-prosecco@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/zonin-prosecco@2x.jpg',
         linkUrl: '',
         price: '$13.99',
         text: 'Zonin Prosecco'
       },
       {
         id: 2,
-        imageUrl: 'https://www.christianhalsell.com/images/billecart-salmon-brut-rose@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/billecart-salmon-brut-rose@2x.jpg',
         linkUrl: '',
         price: '$79.99',
         text: 'Billecart-Salmon Brut Rose'
       },
       {
         id: 3,
-        imageUrl: 'https://www.christianhalsell.com/images/freixenet-cordon-negro-cava-brut@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/freixenet-cordon-negro-cava-brut@2x.jpg',
         linkUrl: '',
         price: '$11.99',
         text: 'Freixenet Cordon Negro Cava Brut'
       },
       {
         id: 4,
-        imageUrl: 'https://www.christianhalsell.com/images/veuve-clicquot-brut-rose@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/veuve-clicquot-brut-rose@2x.jpg',
         linkUrl: '',
         price: '$69.97',
         text: 'Veuve Clicquot Brut Rose'
       },
       {
         id: 5,
-        imageUrl: 'https://www.christianhalsell.com/images/nicolas-feuillatte-reserve-exclusive-brut@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/nicolas-feuillatte-reserve-exclusive-brut@2x.jpg',
         linkUrl: '',
         price: '$29.99',
         text: 'Nicolas Feuillatte Reserve Exclusive Brut'
       },
       {
         id: 6,
-        imageUrl: 'https://www.christianhalsell.com/images/billecart-salmon-brut-reserve@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/billecart-salmon-brut-reserve@2x.jpg',
         linkUrl: '',
         price: '$59.99',
         text: 'Billecart-Salmon Brut reserve'
       },
       {
         id: 7,
-        imageUrl: 'https://www.christianhalsell.com/images/veuve-clicquot-yellow-label-brut@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/veuve-clicquot-yellow-label-brut@2x.jpg',
         linkUrl: '',
         price: '$34.97',
         text: 'Veuve Clicquot Yellow Label Brut'
       },
       {
         id: 8,
-        imageUrl: 'https://www.christianhalsell.com/images/chandon-brut@2x.jpg',
+        imageUrl: 'https://www.christianhalsell.com/images/wine-a-little/chandon-brut@2x.jpg',
         linkUrl: '',
         price: '$17.99',
         text: 'Chandon Brut'
